ebb74fa29baec06acfba60d5bd235fef60343e9f
$H!NDGEKYUME <shindgewongxj@whatsin.ltd>
2023-02-28 10:22:03 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Shindge Wong XJ on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
