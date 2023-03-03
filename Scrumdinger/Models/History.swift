d8960c307ff4f992fdb34fd6754b6e6a406ad30a
$H!NDGEKYUME <shindgewongxj@whatsin.ltd>
2023-03-03 16:07:43 +0800

    Added transcribing speech to text.

--- Scrumdinger/Models/History.swift
+++ Scrumdinger/Models/History.swift
@@ -12,11 +12,13 @@ struct History: Identifiable, Codable {
     let date: Date
     var attendees: [DailyScrum.Attendee]
     var lengthInMinutes: Int
+    var transript: String?
 
-    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5, transript: String? = nil) {
         self.id = id
         self.date = date
         self.attendees = attendees
         self.lengthInMinutes = lengthInMinutes
+        self.transript = transript
     }
 }
